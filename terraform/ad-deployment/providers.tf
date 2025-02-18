aef6d39a7a1739fea3d4c9e1450316de222240dd
4renwald <arnaud@arenwald.com>
2025-02-17 22:14:05 -0500

    ad-deployment action

--- terraform/ad-deployment/providers.tf
+++ terraform/ad-deployment/providers.tf
@@ -14,10 +14,6 @@ terraform {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "2.30.0"
-    }
   }
 
   backend "azurerm" {
