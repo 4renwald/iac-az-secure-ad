b6019642ee22f5d3f4069327570f03476bd8e58b
4renwald <arnaud@arenwald.com>
2025-02-18 08:55:55 -0500

    Edited github env permissions

--- terraform/modules/github-repository-environment/main.tf
+++ terraform/modules/github-repository-environment/main.tf
@@ -6,9 +6,6 @@ resource "github_repository_environment" "repository_environment" {
   environment         = var.environment
   repository          = var.repository
   prevent_self_review = false
-  reviewers {
-    users = [data.github_user.current.id]
-  }
   deployment_branch_policy {
     protected_branches     = true
     custom_branch_policies = false
