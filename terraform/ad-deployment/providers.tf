f2376c6299b404301d070a13144fac58ecb32f75
4renwald <arnaud@arenwald.com>
2025-02-17 22:29:11 -0500

    ad-deployment action

--- terraform/ad-deployment/providers.tf
+++ terraform/ad-deployment/providers.tf
@@ -19,12 +19,14 @@ terraform {
   backend "azurerm" {
     key      = "terraform.tfstate"
     use_oidc = true
+    use_azuread_auth = true
   }
 }
 
 provider "azurerm" {
   features {}
   use_oidc = true
+  
 }
 
 provider "azapi" {
